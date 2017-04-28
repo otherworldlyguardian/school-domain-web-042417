d104bb2d005261343b4a5574f5cee7e17c6d979a
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-28 08:34:42 -0400

    Roll call

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@roster = {}
+  end
+
+  def roster
+    @@roster
+  end
+
+  def add_student(name, grade)
+    if @@roster.keys.include?(grade)
+      @@roster[grade] << name
+    else
+      @@roster[grade] = [name]
+    end
+  end
+
+  def grade(g)
+    @@roster[g]
+  end
+
+  def sort
+    @@roster.each do |grade, array|
+      @@roster[grade] = array.sort
+    end
+  end
+
+end
